5eadfcbbe47853700b76c48f970499318685e5df
Nickaël Rakotonindrina <nrakotonindrina@icebergdatalab.com>
2025-04-16 15:46:33 +0200

    [main] updated light theme

--- wezterm/themes/nord-light.lua
+++ wezterm/themes/nord-light.lua
@@ -2,20 +2,20 @@ return {
   colors = {
     foreground = "#3B4252",
     background = "#ECEFF4",
-    cursor_bg = "#5E81AC",
+    cursor_bg = "#008374",
     cursor_fg = "#ECEFF4",
-    cursor_border = "#5E81AC",
+    cursor_border = "#008374",
     selection_fg = "#2E3440",
     selection_bg = "#D8DEE9",
     scrollbar_thumb = "#A3BE8C",
     split = "#A3BE8C",
     ansi = {
-      "#4C566A", "#BF616A", "#A3BE8C", "#EBCB8B",
-      "#5E81AC", "#B48EAD", "#88C0D0", "#ECEFF4"
+      "#4C566A", "#BF616A", "#A3BE8C", "#ffb900",
+      "#008374", "#B48EAD", "#88C0D0", "#ECEFF4"
     },
     brights = {
-      "#2E3440", "#BF616A", "#A3BE8C", "#EBCB8B",
-      "#5E81AC", "#B48EAD", "#8FBCBB", "#D8DEE9"
+      "#2E3440", "#BF616A", "#A3BE8C", "#ffb900",
+      "#008374", "#B48EAD", "#8FBCBB", "#D8DEE9"
     },
   }
 }
